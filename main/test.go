97da0d0adf978ff243866487aee85c038924f278
IN22912933 <IN22912933>
2024-01-04 01:59:33 +0530

    Initial commit

---
+++ main/test.go
@@ -0,0 +1,18 @@
+
+package main
+
+import "fmt"
+var variable string = "Jojo"
+func main(){
+	fmt.Println("Hello world")
+	
+	var name string  = "cent"
+	var nameno = 2323
+
+	// used for first time import
+	name3 := "afafsd"
+
+	fmt.Println(name)
+	fmt.Println(nameno)
+	fmt.Println(name, nameno, name3, variable)
+}
